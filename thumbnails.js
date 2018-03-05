d1a687237a12d6f3b0e8a3d51106c42f1f6e650e
fara <farasylvain1977@hotmail.com>
2018-03-05 11:39:19 +0900

    Get the items details

--- thumbnails.js
+++ thumbnails.js
@@ -1,11 +1,11 @@
 window.onload = initPage;
-var request;
 
 function initPage(){
 	
 //find the thumbnails on the page
-var thumbs = document.getElementById('thumbnailPane').getElementsByTagName('img');
-
+ thumbs = document.getElementById('thumbnailPane').getElementsByTagName('img');
+ 
+ 
  //set the handler for each image
  for(var i =0; i < thumbs.length; i++){
 	  
@@ -13,10 +13,11 @@ var thumbs = document.getElementById('thumbnailPane').getElementsByTagName('img'
 	  
 	  //create the onclick function
 	  image.onclick = function(){
-		  
+		  alert('hello');
 		  //find the full-size image name 
 		  var detailURL = 'images/' + this.title + '-detail.jpg';
-		  
+		 // itemName = image.title; 
+		  //alert(itemName);
 		  document.getElementById('itemDetail').src = detailURL;
 		  
 		
@@ -28,50 +29,3 @@ var thumbs = document.getElementById('thumbnailPane').getElementsByTagName('img'
 
 }	
 
-function createRequests(){
-	try{
-		
-	request = new XMLHttpRequest();
-
- 			
-	}catch(tryMs){
-		try{
-	request = new ActiveXObject('Msxml2.XMLHTTP');
-		} catch(otherMS){
-			try{
-				
-	request = new ActiveXObject('Microsoft.XMLHTTP');			
-			}catch(failed){
-			
-          request = null;			
-				
-			}
-			
-			
-		}
-		
-	}
-	return request;
-	
-}
-
-function getDetails(itemName){
-	
-	request = createRequests();
-	
-	if(request == null){
-	alert('unable to create request');
-	return;
-	}
-	
-	var url = 'getDetails.php?ImageID=' + escape(itemName);
-	request.open('GET', url, true);
-	request.onreadystatechange = displayDetails;
-	request.send(null);
-	
-	
-	
-	
-}
-
-getDetails(itemName);
